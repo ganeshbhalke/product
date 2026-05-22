a1bd14cd96da4c672d091a6378022d48a0834044
ganeshbhalke <ganeshbhalke2004@gmail.com>
2026-05-22 16:46:36 +0530

    Read&create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProductDashboardComponent } from './shared/component/product-dashboard/product-dashboard.component';
+import { ProductFormComponent } from './shared/component/product-form/product-form.component';
+import { ProductListComponent } from './shared/component/product-list/product-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule } from "@angular/material/button"
+import {MatIconModule } from "@angular/material/icon"
+import {MatSnackBarModule } from "@angular/material/snack-bar"
+import {FormsModule } from "@angular/forms"
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDashboardComponent,
+    ProductFormComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/Services/product-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductServiceService {
+
+  constructor(
+    private _snackBar:MatSnackBar
+  ) { }
+
+
+
+  openSnackBar(msg:string){
+    this._snackBar.open(msg,'Close',{
+      horizontalPosition:'left',
+      verticalPosition:'top',
+      duration:3000
+    }
+      
+    )
+  }
+
+
+}
